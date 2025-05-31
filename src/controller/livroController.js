0c394700008b5bac315832e3d644a0c6f6bc0b92
Daya Souza <dayanalvesouza@gmail.com>
2025-05-31 20:41:47 -0300

    Adicionando novas funcionalidades a api

--- src/controller/livroController.js
+++ src/controller/livroController.js
@@ -1,4 +1,5 @@
 import livro from "../models/Livro.js";
+import { autor } from "../models/Autor.js"
 
 class LivroController {
   static async listarLivros(req, res) {
@@ -25,8 +26,12 @@ class LivroController {
   }
 
   static async cadastrarLivro(req, res) {
+    const novoLivro = req.body;
+
     try {
-      const novoLivro = await livro.create(req.body);
+      const autorEncontrado = await autor.findById(novoLivro.autor);
+      const livroCompleto = { ...novoLivro, autor: { ...autorEncontrado._doc } };
+      const livroCriado = await livro.create(livroCompleto);
       res.status(201).json({ message: "criado com sucesso", livro: novoLivro });
     } catch (erro) {
       res
@@ -54,6 +59,18 @@ class LivroController {
       res.status(200).json({ message: "livro excluído com sucesso!" });
     } catch (erro) {
       res.status(500).json({ message: `${erro.message} - falha na exclusão` });
+    }
+  }
+
+  static async listarLivrosPorEditora(req, res){
+    const editora = req.query.editora;
+    try {
+      const livrosPorEditora = await livro.find({ editora: editora });
+      res.status(200).json(livrosPorEditora);
+    } catch (erro) {
+      res
+        .status(500)
+        .json({ message: `${erro.message} - falha na busca` });
     }
   }
 }
